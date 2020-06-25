2675d2f2086ec63af4a0a94830b4ff8f72483377
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-24 23:51:24 -0400

    FirstCommit

---
+++ src/AnArraytoRemember.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class AnArraytoRemember {
+    public static void main(String[] args){
+        String [] tenPresidents = {"Johnson", "Nixon", "Ford", "Carter", "Reagan", "Bush","Clinton", "Bush", "Obama", "Trump"};
+
+        System.out.println(tenPresidents[9]);
+        System.out.println(tenPresidents[8]);
+        System.out.println(tenPresidents[7]);
+        System.out.println(tenPresidents[6]);
+        System.out.println(tenPresidents[5]);
+        System.out.println(tenPresidents[4]);
+        System.out.println(tenPresidents[3]);
+        System.out.println(tenPresidents[2]);
+        System.out.println(tenPresidents[1]);
+        System.out.println(tenPresidents[0]);
+
+
+    }
+}
