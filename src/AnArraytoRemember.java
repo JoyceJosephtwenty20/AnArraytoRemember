a4b14d64be70829a5640c0a031bcfbd6a8519bbb
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-25 00:31:09 -0400

    FixedArraytouseLoop

--- src/AnArraytoRemember.java
+++ src/AnArraytoRemember.java
@@ -1,20 +1,20 @@
 import java.util.Arrays;
 
 public class AnArraytoRemember {
-    public static void main(String[] args){
-        String [] tenPresidents = {"Johnson", "Nixon", "Ford", "Carter", "Reagan", "Bush","Clinton", "Bush", "Obama", "Trump"};
+    public static void main(String[] args) {
+        String[] tenPresidents = {"Johnson", "Nixon", "Ford", "Carter", "Reagan", "Bush", "Clinton", "Bush", "Obama", "Trump"};
 
-        System.out.println(tenPresidents[9]);
-        System.out.println(tenPresidents[8]);
-        System.out.println(tenPresidents[7]);
-        System.out.println(tenPresidents[6]);
-        System.out.println(tenPresidents[5]);
-        System.out.println(tenPresidents[4]);
-        System.out.println(tenPresidents[3]);
-        System.out.println(tenPresidents[2]);
-        System.out.println(tenPresidents[1]);
-        System.out.println(tenPresidents[0]);
+        System.out.println("Original Array: " + Arrays.toString(tenPresidents));
+
+        for (int i = tenPresidents.length - 1; i >= 0; i--) {
+            System.out.println(tenPresidents[i]);
+        }
 
 
     }
 }
+
+
+
+
+
